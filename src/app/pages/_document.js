90aa642ebad814b38c0c2c1f6cf4c5104884d625
XnUnknown <nishantvishwakarma6789@gmail.com>
2025-03-16 16:51:37 +0530

    Changed the META tags for SEO

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -7,7 +7,8 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Big+Shoulders:opsz,wght@10..72,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Tektur:wght@400..900&display=swap" rel="stylesheet" />
-
+            <title>XnUnknown - Portfolio</title>
+            <meta name="description" content="Welcome to my portfolio. Showcasing my work in web development, AI, VFX, and more." />
       </Head>
       <body>
         <Main />
