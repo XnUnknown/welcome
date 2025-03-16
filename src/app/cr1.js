a66021b89e65d92535790ccb9376af65578d529b
XnUnknown <nishantvishwakarma6789@gmail.com>
2025-03-16 17:08:05 +0530

    Changed All the Comma

--- src/app/cr1.js
+++ src/app/cr1.js
@@ -9,9 +9,9 @@ const cards = () => {
                     <h2>Profile</h2>
                 </div>
                 <div className={style.des}>
-                    <p>Hey&lsquo; I&apos;m Nishant Vishwakarma—a tech enthusiast&lsquo; full-stack developer in the making&lsquo; and a creative mind passionate about AI&lsquo; VFX&lsquo; and web development. Currently pursuing B.Tech in Computer Science at KIET&lsquo; I&apos;m on a journey to push my skills in coding&lsquo; design&lsquo; and engineering.
+                    <p>Hey, I&apos;m Nishant Vishwakarma—a tech enthusiast, full-stack developer in the making, and a creative mind passionate about AI, VFX, and web development. Currently pursuing B.Tech in Computer Science at KIET, I&apos;m on a journey to push my skills in coding, design, and engineering.
 
-I love building things—whether it&apos;s web apps&lsquo; animations&lsquo; or AI-powered projects. My interests range from 3D modeling in Blender to video editing and VFX&lsquo; blending tech and creativity in unique ways. I&apos;ve dabbled in Arduino projects&lsquo; explored speech recognition & AI integration&lsquo; and I&apos;m always looking for new challenges to tackle.</p>
+I love building things—whether it&apos;s web apps, animations, or AI-powered projects. My interests range from 3D modeling in Blender to video editing and VFX, blending tech and creativity in unique ways. I&apos;ve dabbled in Arduino projects, explored speech recognition & AI integration, and I&apos;m always looking for new challenges to tackle.</p>
                 </div>
             </div>
             <div className={style.newcardright}>
